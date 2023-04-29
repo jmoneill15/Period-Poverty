22835dede1a680024fc84576493363163120765d
jmoneill15 <124748245+jmoneill15@users.noreply.github.com>
2023-04-29 12:56:17 -0400

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App(): JSX.Element {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Period Poverty
+      </header>
+  </div>
+);
+}
+export default App;
